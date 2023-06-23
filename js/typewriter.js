d53bfc538d5c69c7e773c7fb2a6431b5e63e95ec
Woojin <wjeo779@aucklanduni.ac.nz>
2023-06-23 14:09:37 +1200

    Icons are located in the middle

--- js/typewriter.js
+++ js/typewriter.js
@@ -12,13 +12,27 @@ function typeWriter(element, text, i) {
   }
 }
 
-const h1 = document.querySelector('h1.typewriter');
-const h2 = document.querySelector('h2.typewriter');
+window.onload = function() {
+    setTimeout(function() {
+        const h1 = document.querySelector('h1.typewriter');
+        const h2 = document.querySelector('h2.typewriter');
+        const h3 = document.querySelector('h3.typewriter'); 
 
-const text1 = 'WOOJIN\nJEON';
-h1.innerHTML = '';
-typeWriter(h1, text1, 0);
 
-const text2 = 'AKL,NZ';
-h2.textContent = '';
-setTimeout(() => typeWriter(h2, text2, 0), speed * text1.length);
+
+        const text1 = 'WOOJIN\nJEON';
+        h1.innerHTML = '';
+        typeWriter(h1, text1, 0);
+
+        const text2 = 'AKL,NZ';
+        h2.textContent = '';
+        setTimeout(() => typeWriter(h2, text2, 0), speed * text1.length);
+
+        const text3 = 'Developer'; 
+        h3.textContent = ''; 
+        setTimeout(() => typeWriter(h3, text3, 0), speed * text1.length); 
+    }, 200);
+
+
+        
+};
