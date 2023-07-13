1f34f9b5840f26401f5e1979706f881a4ebb6586
Woojin <wjeo779@aucklanduni.ac.nz>
2023-07-13 18:53:36 +1200

    Added overlay for better navigation of the website

---
+++ js/header.js
@@ -0,0 +1,34 @@
+var overlay = document.getElementById("menuOverlay");
+var btn = document.getElementById("toggleButton");
+var specifiedElement = document.getElementById('overlay')
+var count=0;
+
+document.addEventListener('click', event => {
+    
+    const isClickInside = specifiedElement.contains(event.target)
+  
+    if (!isClickInside && overlay.style.height == "100%" && count>0) {
+      
+        overlay.style.height = "0%";
+        btn.innerHTML = '<i class="fa-solid fa-bars fa-2xl"></i>';
+      
+    }
+    count++;
+  })
+
+
+function toggleNav() {
+    
+    if (overlay.style.height == "100%") {
+        overlay.style.height = "0%";
+        btn.innerHTML = '<i class="fa-solid fa-bars fa-2xl"></i>';
+    } else {
+        overlay.style.height = "100%";
+        btn.innerHTML = '<i class="fa-solid fa-x fa-2xl"></i>';
+        count = 0;
+        btn.style.backgroundColor = "#00000000";
+
+    }
+}
+
+
